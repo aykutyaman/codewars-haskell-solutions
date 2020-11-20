bc2384235dc7c7a57ef991a4b5bc7d415573218e
aykut <aykut@jotform.com>
2020-11-20 19:36:35 +0300

    remove fromIntegral

--- src/GrowthOfPopulation.hs
+++ src/GrowthOfPopulation.hs
@@ -1,7 +1,9 @@
 module GrowthOfPopulation where
 
+-- https://www.codewars.com/kata/563b662a59afc2b5120000c6
+
 nbYear :: Int -> Double -> Int -> Int -> Int
-nbYear p0 percent aug p = go 0 (fromIntegral p0)
+nbYear p0 percent aug p = go 0 p0
   where
     go :: Int -> Int -> Int
     go acc x
